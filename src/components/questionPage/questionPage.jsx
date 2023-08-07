3d66cecb242e578b038f817bf9d21b7da95bdfc1
Namrath <namrathracharla1@gmail.com>
2023-08-07 23:04:36 +0530

    responsive questionPage

--- src/components/questionPage/questionPage.jsx
+++ src/components/questionPage/questionPage.jsx
@@ -3,30 +3,33 @@ import React from 'react'
 function QuestionPage() {
     return (
         <div>
-            <div class="px-12">
-            <div class="max-w-[85%] min-h-screen rounded md:rounded-full bg-[#c0c0c065] m-auto  mb-0 pb-10">
-            <div class="font-Helvectica text-center max-w-[340px] m-auto flex-col overflow-hidden flex-wrap">
-                <h3 class="font-semibold text-2xl pt-32">Select the one thing you value most right now.</h3>
-                <div class="h-[70px] max-w-[340px] px-[15px] pt-[25px] shadow-md bg-white my-5  rounded-xl ">
+            <div class="px-3 border-8 border-[#c0c0c082]">   
+            <div class="max-w-[95%] max-h-screen rounded md:rounded-full bg-[#c0c0c050] m-auto md:max-w-[85%]">
+                <div class="font-Helvectica text-center max-w-[250px] m-auto flex-col overflow-hidden flex-wrap  md:max-w-[340px] relative">
+                <h3 class="font-semibold text-lg pt-20 md:text-2xl ">Select the one thing you value most right now.</h3>
+                <div class="h-[40px] max-w-[190px] px-[15px] pt-[9px] pb-[3px] shadow-md  bg-white mt-10 mx-auto rounded-xl text-sm  md:h-[70px] md:max-w-[340px] md:px-[15px] md:pt-[18px] md:pb-[4px] md:text-lg border-4 border-[#c0c0c065] hover:bg-[#ffffffc4]">
                     Text
                 </div>
-                <div class="h-[70px] max-w-[340px] px-[15px] py-[25px] shadow-md  bg-white my-5 rounded-xl">
+                <div class="h-[40px] max-w-[190px] px-[15px] pt-[9px] pb-[3px] shadow-md  bg-white my-4 mx-auto rounded-xl text-sm  md:h-[70px] md:max-w-[350px] md:px-[15px] md:pt-[18px] md:pb-[4px] md:text-lg border-4 border-[#c0c0c065] hover:bg-[#ffffffc4]">
                     Text
                 </div>
-                <div class="h-[70px] max-w-[340px] px-[15px] py-[25px] shadow-md  bg-white my-5 rounded-xl">
+                <div class="h-[40px] max-w-[190px] px-[15px] pt-[9px] pb-[3px] shadow-md  bg-white my-4 mx-auto rounded-xl text-sm  md:h-[70px] md:max-w-[350px] md:px-[15px] md:pt-[18px] md:pb-[4px] md:text-lg border-4 border-[#c0c0c065] hover:bg-[#ffffffc4]">
                     Text
                 </div>
-                <div class="h-[70px] max-w-[340px] px-[15px] py-[25px] shadow-md  bg-white my-5 rounded-xl">
+                <div class="h-[40px] max-w-[190px] px-[15px] pt-[9px] pb-[3px] shadow-md  bg-white my-4 mx-auto rounded-xl text-sm  md:h-[70px] md:max-w-[350px] md:px-[15px] md:pt-[18px] md:pb-[4px] md:text-lg border-4 border-[#c0c0c065] hover:bg-[#ffffffc4]">
                     Text
                 </div>
-                <div class="h-[70px] max-w-[340px] px-[15px] py-[25px] shadow-md  bg-white my-5 rounded-xl">
+                <div class="h-[40px] max-w-[190px] px-[15px] pt-[9px] pb-[3px] shadow-md  bg-white my-4 mx-auto rounded-xl text-sm  md:h-[70px] md:max-w-[350px] md:px-[15px] md:pt-[18px] md:pb-[4px] md:text-lg border-4 border-[#c0c0c065] hover:bg-[#ffffffc4]">
                     Text
                 </div>
-                <div class="h-[70px] max-w-[340px] px-[15px] py-[25px] shadow-md  bg-white my-5 rounded-xl">
+                <div class="h-[40px] max-w-[190px] px-[15px] pt-[9px] pb-[3px] shadow-md  bg-white my-4 mx-auto rounded-xl text-sm  mb-10 md:h-[70px] md:max-w-[350px] md:px-[15px] md:pt-[18px] md:pb-[4px] md:text-lg border-4 border-[#c0c0c065] hover:bg-[#ffffffc4]">
                     Text
                 </div>
-            </div>  
+                
+            </div> 
+           
             </div>
+            
             </div>
         </div>
     )
